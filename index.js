a63567ed992ea13eed57c680061f3187d3dc3655
Rodrigo Bunhak Zwierzikowski <rodrigo.a@novacki.local>
2020-06-17 09:42:33 -0300

    Challenge 1.3 | Functions and loop structures | Search by technologie

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-// Users and technologies
+// Search by technologie
 const users = [
   { name: 'Carlos', technologies: ['HTML', 'CSS'] },
   { name: 'José', technologies: ['Javascript', 'CSS'] },
   { name: 'Tuane', technologies: ['C#', 'Java'] },
-  { name: 'Camila', technologies: ['Node.js', 'React'] }
+  { name: 'Camila', technologies: ['Node.js', 'React', 'CSS'] }
 ]
 
-for(i = 0; i < users.length; i++) {
-  console.log(`${users[i].name} trabalha com ${users[i].technologies}`)
+function checkUserUseCSS(user) {
+  for (let i = 0; i < user.technologies.length; i++) {
+    if (user.technologies[i] === 'CSS') {
+      return true
+    }
+  }
+  return false
+}
+
+for (let i = 0; i < users.length; i++) {
+  const userWorkWithCSS = checkUserUseCSS(users[i])
+
+  if (userWorkWithCSS) {
+    console.log(`User ${users[i].name} work with CSS.`)
+  }
 }
