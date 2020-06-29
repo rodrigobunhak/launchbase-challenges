b1503f4fae6c8042591b246f73b972627ed1b240
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-06-28 22:31:12 -0300

    Challenge 3-1 | First Server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const server = express()
+const nunjucks = require('nunjucks')
+
+server.set("view engine", "njk")
+nunjucks.configure("views", {
+  express: server
+})
+
+server.use(express.static('public'))
+
+server.get("/", function(req, res) {
+  return res.render("courses")
+})
+
+server.get("/about", function(req, res) {
+  return res.render("about")
+})
+
+server.use(function(req, res) {
+  res.status(404).render("not-found")
+})
+
+server.listen(5000, function(){
+  console.log('Server is running!')
+})
