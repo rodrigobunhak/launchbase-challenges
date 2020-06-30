e64fec8e9406c956d7c30364a2a9c65dea3b8bbc
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-06-30 11:33:00 -0300

    Challenge 4-1 | Header

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const server = express()
 
-const courses = require('./data')
-
-
 server.set("view engine", "njk")
 nunjucks.configure("views", {
   express: server,
@@ -14,35 +11,13 @@ nunjucks.configure("views", {
 server.use(express.static('public'))
 
 server.get("/", function(req, res) {
-  return res.render("courses", {courses})
+  return res.render("layout")
 })
 
-server.get("/about", function(req, res) {
-  return res.render("about")
+server.get("/students", function(req, res) {
+  return res.render("layout")
 })
 
-server.get("/course", function(req, res) {
-  const id = req.query.id
-  console.log(id)
-
-  const course = courses.find(function(course){
-    if (course.id == id) {
-      return true
-    }
-  })
-
-  if (!course) {
-    return res.send("course not found!")
-  }
-
-  return res.render("course", {course})
-
-})
-
-
-server.use(function(req, res) {
-  res.status(404).render("not-found")
-})
 
 server.listen(5000, function(){
   console.log('Server is running!')
