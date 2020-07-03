6cd71acbd0217fac60e87dfdc305b3cebdf91a0b
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-07-03 19:47:09 -0300

    Challenge 4-3 | Form e Teacher register route

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
+
 const server = express()
 
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static('public'))
+server.use(routes)
+
 server.set("view engine", "njk")
+
 nunjucks.configure("views", {
   express: server,
+  autoescape: false,
   noCache: true
 })
-
-server.use(express.static('public'))
-
-server.get("/", function(req, res) {
-  return res.render("teachers/index")
-})
-
-server.get("/students", function(req, res) {
-  return res.render("layout")
-})
-
 
 server.listen(5000, function(){
   console.log('Server is running!')
---
+++ teachers.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const data= require("./data.json")
+//create
+exports.post = function (req, res) {
+  // req.body
+
+  const keys = Object.keys(req.body)
+
+  for (key of keys) {
+    if (req.body[key] == "") {
+      return res.send('Please, fill all fields!')
+    }
+  }
+
+  data.teachers.push(req.body)
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if (err) return res.send("Write file error!")
+
+    return res.redirect("/teachers")
+  })
+}
